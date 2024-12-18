931b27bceeb423275beec65cb4e08559d0d328de
koteyka1489 <kolokolchikov.ds@gmail.com>
2024-12-18 22:33:02 +0300

    start tests

--- Source/LinkedList.h
+++ Source/LinkedList.h
@@ -3,158 +3,151 @@
 #include <vector>
 #include <string>
 
-
-template<typename T>
+template <typename T>
 struct Node
 {
-	explicit Node(T data) : Data(data) {}
-	Node<T>* Next = nullptr;
-	T Data;
+    explicit Node(T data) : Data(data) {}
+    Node<T>* Next = nullptr;
+    T Data;
 };
 
-
-
-template<typename T>
+template <typename T>
 class LinkedList
 {
 public:
+    LinkedList() = default;
 
+    template <typename U>
+        requires std::same_as<T, U>
+    LinkedList(U data)
+    {
+        Head = new Node<U>(data);
+        ++Num;
+    }
 
-	LinkedList() = default;
+    template <typename U>
+        requires std::same_as<T, U>
+    LinkedList(std::initializer_list<U> args)
+    {
+        for (const auto& arg : args)
+        {
+            this->PushBack(arg);
+        }
+    }
 
-	template<typename U>
-	requires std::same_as<T, U>
-	LinkedList(U data) {
-		Head = new Node<U>(data);
-		++Num;
-	}
+    ~LinkedList()
+    {
+        while (Head)
+        {
+            auto Temp = Head;
+            Head = Temp->Next;
+            delete Temp;
+        }
+    }
 
-	template<typename U>
-	requires std::same_as<T, U>
-	LinkedList(std::initializer_list<U> args) {
-		for (const auto& arg : args) {
-			this->PushBack(arg);
-		}
-	}
+    template <typename U>
+        requires std::same_as<T, U>
+    void PushBack(U data)
+    {
+        Node<U>* NewNode = new Node<U>(data);
+        if (Head == nullptr)
+        {
+            Head = new Node<U>(data);
+        }
+        else if (Head->Next == nullptr)
+        {
+            Head->Next = NewNode;
+            Tail = NewNode;
+        }
+        else
+        {
+            Tail->Next = NewNode;
+            Tail = NewNode;
+        }
+        ++Num;
+    }
 
-	~LinkedList() {
-		while (Head) {
-			auto Temp = Head;
-			Head = Temp->Next;
-			delete Temp;
-		}
-	}
+    void PopBack()
+    {
+        if (Head == nullptr || Num == 0) return;
 
-	template<typename U>
-	requires std::same_as<T, U>
-	void PushBack(U data) {
-		Node<U>* NewNode = new Node<U>(data);
-		if (Head == nullptr) {
-			Head = new Node<U>(data);
-		}
-		else if (Head->Next == nullptr)	{
-			Head->Next = NewNode;
-			Tail = NewNode;
-		}
-		else {
-			Tail->Next = NewNode;
-			Tail = NewNode;
-		}
-		++Num;
-	}
+        if (Tail == nullptr || Num == 1)
+        {
+            delete Head;
+            Head = nullptr;
+        }
+        else
+        {
+            if (Num > 2)
+            {
+                auto Current = Head;
+                while (Current->Next != Tail)
+                {
+                    Current = Current->Next;
+                }
+                Current->Next = nullptr;
+                delete Tail;
+                Tail = Current;
+            }
+            else
+            {
+                Head->Next = nullptr;
+                delete Tail;
+                Tail = nullptr;
+            }
+        }
+        --Num;
+    }
 
-	void PopBack() {
-		if (Head == nullptr || Num == 0) return;
+    int GetNum() { return Num; }
 
-		if (Tail == nullptr || Num == 1) {
-			delete Head;
-			Head = nullptr;
-		}
-		else
-		{
-			if (Num > 2) {
-				auto Current = Head;
-				while (Current->Next != Tail)
-				{
-					Current = Current->Next;
-				}
-				Current->Next = nullptr;
-				delete Tail;
-				Tail = Current;
-			}
-			else
-			{
-				Head->Next = nullptr;
-				delete Tail;
-				Tail = nullptr;
-			}
-		}
-		--Num;
-	}
+    bool IsEmpty() { return Num == 0; }
 
-	int GetNum() {
-		return Num;
-	}
+    void Empty()
+    {
+        while (Head)
+        {
+            auto Temp = Head;
+            Head = Temp->Next;
+            delete Temp;
+        }
+        Num = 0;
+    }
 
-	bool IsEmpty() {
-		return Num == 0;
-	}
-
-	void Empty() {
-		while (Head) {
-			auto Temp = Head;
-			Head = Temp->Next;
-			delete Temp;
-		}
-		Num = 0;
-	}
 private:
-	Node<T>* Head = nullptr;
-	Node<T>* Tail = nullptr;
-	int Num = 0;
-
+    Node<T>* Head = nullptr;
+    Node<T>* Tail = nullptr;
+    int Num = 0;
 
 public:
+    // ITERATORS
+    class Iterator
+    {
+    public:
+        Node<T>* Current;
 
-	// ITERATORS
-	class Iterator
-	{
-	public:
-		Node<T>* Current;
+        Iterator(Node<T>* node) : Current(node) {}
 
-		Iterator(Node<T>* node) : Current(node)	{ }
+        T& operator*() { return Current->Data; }
 
-		T& operator*() {
-			return Current->Data;
-		}
+        Iterator operator++()
+        {
+            Current = Current->Next;
+            return *this;
+        }
 
-		Iterator operator++ () {
-			Current = Current->Next;
-			return *this;
-		}
+        Iterator operator++(int)
+        {
+            Iterator Temp = *this;
+            Current = Current->Next;
+            return Temp;
+        }
 
-		Iterator operator++ (int) {
-			Iterator Temp = *this;
-			Current = Current->Next;
-			return Temp;
-		}
+        bool operator!=(const Iterator& rhs) const { return Current != rhs.Current; }
 
-		bool operator!=(const Iterator& rhs) const {
-			return Current != rhs.Current;
-		}
+        bool operator==(const Iterator& rhs) const { return Current == rhs.Current; }
+    };
 
-		bool operator==(const Iterator& rhs) const {
-			return Current == rhs.Current;
-		}
-	};
-	
-	Iterator begin() {
-		return Iterator(Head);
-	}
-	Iterator end() {
-		return Iterator(nullptr);
-	}
-
+    Iterator begin() { return Iterator(Head); }
+    Iterator end() { return Iterator(nullptr); }
 };
-
-
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <concepts>
-#include <vector>
-#include <string>
 #include "LinkedList.h"
 
 
