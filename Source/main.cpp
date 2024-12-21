8e28dfc70d969c5f14d6f7e774376f403275dca5
koteyka1489 <kolokolchikov.ds@gmail.com>
2024-12-21 22:53:02 +0300

    add tests

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,39 +4,5 @@
 int main()
 {
 
-    LinkedList<int> List(1);
-    List.PushBack(2);
-    List.PushBack(3);
-    List.PushBack(4);
-    List.PushBack(5);
-    List.PopBack();
-    List.PopBack();
-
-    LinkedList<float> List2{1.0555f, 2.2f, 3.3f, 4.4f, 5.5f};
-
-    List2.Empty();
-
-    LinkedList<char> List3{'p', 'i', 'z', 'd', 'a'};
-
-    std::cout << "List Num - " << List.GetNum() << "\nmembers :\n";
-    for (auto& l : List)
-    {
-        std::cout << l << "\n";
-    }
-
-    std::cout << "List2 Num - " << List2.GetNum() << "\nmembers :\n";
-    for (auto& l : List2)
-    {
-        std::cout << l << "\n";
-    }
-
-    std::cout << "List3 Num - " << List3.GetNum() << "\nmembers :\n";
-    for (auto& l : List3)
-    {
-        std::cout << l << "\n";
-    }
-
-    getchar();
-
     return 0;
 }
--- Tests/ListTest.cpp
+++ Tests/ListTest.cpp
@@ -4,16 +4,16 @@
 TEST(LinkedListTests, CreateList)
 {
     LinkedList<int> List (1);
-    ASSERT_TRUE(List.GetNum() == 1);
-
+    EXPECT_TRUE(List.GetNum() == 1);
+    
     LinkedList<int> List1 {1, 2, 3, 4, 5, 6};
-    ASSERT_TRUE(List1.GetNum() == 6);
+    EXPECT_TRUE(List1.GetNum() == 6);
 
     LinkedList<int> List2{};
-    ASSERT_TRUE(List2.GetNum() == 0);
+    EXPECT_TRUE(List2.GetNum() == 0);
 
     LinkedList<int> List3;
-    ASSERT_TRUE(List3.GetNum() == 0);
+    EXPECT_TRUE(List3.GetNum() == 0);
 }
 
 TEST(LinkedListTests, PushPopList)
@@ -21,24 +21,31 @@ TEST(LinkedListTests, PushPopList)
     LinkedList<int> List;
 
     List.PopBack();
-    ASSERT_TRUE(List.GetNum() == 0);
+    EXPECT_TRUE(List.GetNum() == 0);
 
     List.PushBack(2);
     List.PushBack(3);
     List.PushBack(4);
     List.PushBack(5);
-    ASSERT_TRUE(List.GetNum() == 4);
+    EXPECT_TRUE(List.GetNum() == 4);
 
     List.PopBack();
     List.PopBack();
     List.PopBack();
-    ASSERT_TRUE(List.GetNum() == 1);
+    EXPECT_TRUE(List.GetNum() == 1);
 
     List.PopBack();
     List.PopBack();
     List.PopBack();
-    ASSERT_TRUE(List.GetNum() == 0);
+    EXPECT_TRUE(List.GetNum() == 0);
 
     List.PushBack(2);
-    ASSERT_TRUE(List.GetNum() == 1);
+    EXPECT_TRUE(List.GetNum() == 1);
+
+    LinkedList<float> List2{1.0555f, 2.2f, 3.3f, 4.4f, 5.5f};
+    List2.Empty();
+    EXPECT_TRUE(List2.GetNum() == 0);
+    EXPECT_TRUE(List2.IsEmpty());
+
+
 }
